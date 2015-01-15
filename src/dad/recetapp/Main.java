c232ea44009cede49c72e77348ff0efcba2510a2
Daniel <dani90ferry@gmail.com>
2015-01-15 00:39:21 +0000

    Primer esbozo de la ventana principal de la aplicación RecetApp. Pestañas realizadas: - Categorías - Ingredientes - Medidas - Anotaciones

---
+++ src/dad/recetapp/Main.java
@@ -0,0 +1,9 @@
+package dad.recetapp;
+
+import org.apache.pivot.wtk.DesktopApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		DesktopApplicationContext.main(RecetAppApplication.class, args);
+	}
+}
---
+++ src/dad/recetapp/ui/RecetAppWindow.java
@@ -0,0 +1,19 @@
+package dad.recetapp.ui;
+
+import java.net.URL;
+
+import org.apache.pivot.beans.BXML;
+import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.ImageView;
+import org.apache.pivot.wtk.Window;
+
+public class RecetAppWindow extends Window implements Bindable {
+	@BXML private ImageView imagen;
+	
+	@Override
+	public void initialize(Map<String, Object> namespace, URL location, Resources resources) {
+		
+	}
+}
