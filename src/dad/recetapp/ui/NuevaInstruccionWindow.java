8a5364034a53dc2ca60cdd5bd17c2d0b0279dbc5
David <eerfati@gmail.com>
2015-01-22 12:21:32 +0000

    He añadido las ventanas de editar y añadir instrucciones.

---
+++ src/dad/recetapp/ui/NuevaInstruccionWindow.java
@@ -0,0 +1,17 @@
+package dad.recetapp.ui;
+
+import java.net.URL;
+
+import org.apache.pivot.beans.Bindable;
+import org.apache.pivot.collections.Map;
+import org.apache.pivot.util.Resources;
+import org.apache.pivot.wtk.Window;
+
+public class NuevaInstruccionWindow extends Window implements Bindable {
+
+	@Override
+	public void initialize(Map<String, Object> namespace, URL location, Resources resources) {
+		
+	}
+
+}
